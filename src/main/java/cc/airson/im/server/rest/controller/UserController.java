5d8b9700eb42a9ffe58523190227aa919c9aa440
airson <airson_yu@163.com>
2021-01-28 17:18:28 +0800

    user api

---
+++ src/main/java/cc/airson/im/server/rest/controller/UserController.java
@@ -0,0 +1,65 @@
+package cc.airson.im.server.rest.controller;
+
+import cc.airson.im.server.rest.handler.UserHandler;
+import cc.airson.im.server.rest.tools.Result;
+import cc.airson.im.server.rest.vo.UserVO;
+import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * TODO
+ *
+ * @author airson
+ */
+@Validated
+@RestController
+@Api(value = "用户相关接口", tags = "user")
+@RequestMapping(value = "/v1/user/")
+public class UserController {
+
+    @Autowired
+    private UserHandler userHandler;
+
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    static {
+        logger.trace("user domain log");
+    }
+
+    @ApiOperation(value = "新增用户", tags = "user")
+    @RequestMapping(value = "add")
+    public JSONObject add(@Valid UserVO userVO, BindingResult result) {
+        logger.debug("received cmd user add:{}", userVO);
+        JSONObject json = new JSONObject();
+        userHandler.addUser(json, userVO);
+        return Result.success_rsp(json, "user_add");
+    }
+
+    @ApiOperation(value = "修改用户", tags = "user")
+    @RequestMapping(value = "update")
+    public JSONObject update(@Valid UserVO userVO, BindingResult result) {
+        logger.debug("received cmd user update:{}", userVO);
+        JSONObject json = new JSONObject();
+        userHandler.updateUser(json, userVO);
+        return Result.success_rsp(json, "user_update");
+    }
+
+    @ApiOperation(value = "加载用户", tags = "user")
+    @RequestMapping(value = "load")
+    public JSONObject load(@RequestParam Long uid) {
+        logger.debug("received user load:{}", uid);
+        JSONObject json = new JSONObject();
+        userHandler.load(json, uid);
+        return Result.success_rsp(json, "user_load");
+    }
+
+}
---
+++ src/main/java/cc/airson/im/server/rest/handler/UserHandler.java
@@ -0,0 +1,111 @@
+package cc.airson.im.server.rest.handler;
+
+import cc.airson.im.server.rest.advice.DomainException;
+import cc.airson.im.server.rest.config.Const;
+import cc.airson.im.server.rest.config.ResponseCode;
+import cc.airson.im.server.rest.controller.UserController;
+import cc.airson.im.server.rest.dao.po.User;
+import cc.airson.im.server.rest.service.UserService;
+import cc.airson.im.server.rest.tools.Result;
+import cc.airson.im.server.rest.tools.SecurityUtil;
+import cc.airson.im.server.rest.vo.*;
+import com.alibaba.druid.util.StringUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * TODO
+ *
+ * @author airson
+ */
+@Component
+public class UserHandler {
+
+    @Autowired
+    private UserService   userService;
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    //private Logger logger = LoggerFactory.getLogger(getClass());
+    private static Logger logger = LoggerFactory.getLogger(UserHandler.class);
+
+    public JSONObject addUser(JSONObject json, UserVO userVO) {
+        User user = new User();
+        user.setUserName(userVO.getUserName());
+        user.setPhone(userVO.getPhone());
+        user.setCreateTime(new Date());
+        userService.insert(user);
+
+        String userKey = "u_" + user.getId();
+        String userData = JSON.toJSONString(user);
+
+        json.put("user", userData);
+        logger.debug("add user:{}", userData);
+
+        redisTemplate.opsForValue().set(userKey, userData);
+        redisTemplate.opsForList().rightPush("ulist", userData);
+        kafkaTemplate.send(Const.MQ_TOPIC_USER, MQMsgBuilder.user_add(user));
+
+        Result.success(json);
+        return json;
+    }
+
+    public JSONObject updateUser(JSONObject json, UserVO userVO) {
+        User user = new User();
+        user.setUserName(userVO.getUserName());
+        user.setPhone(userVO.getPhone());
+        user.setUpdateTime(new Date());
+        userService.update(user);
+        // TODO send kafka message to ws project, ws project save the message
+
+        String userKey = "u_" + user.getId();
+        String userData = JSON.toJSONString(user);
+
+        json.put("user", userData);
+        logger.debug("update user:{}", userData);
+
+        redisTemplate.opsForValue().set(userKey, userData);
+        redisTemplate.opsForList().rightPush("ulist", userData);
+        kafkaTemplate.send(Const.MQ_TOPIC_USER, MQMsgBuilder.user_update(user));
+
+        Result.success(json);
+        return json;
+    }
+
+    public JSONObject deleteUser(JSONObject json, Long id) {
+
+        return json;
+    }
+
+    public JSONObject load(JSONObject json, Long id) {
+        String userKey = "u_" + id;
+        Object userObj = redisTemplate.opsForValue().get(userKey);
+        if (null != userObj) {
+            JSONObject userJson = (JSONObject) userObj;
+            json.put("data", userJson);
+            logger.debug("load user from redis:{}", id);
+        }
+        User user = userService.load(id);
+        if (null != user) {
+            json.put("data", user);
+            logger.debug("load user from db:{}", id);
+        } else {
+            logger.debug("load user null:{}", id);
+        }
+
+        return json;
+    }
+
+}
